82a17e34ddf37d369ad9f081b14ba627e9522692
Yaroslav <yaroslav200124@gmail.com>
2019-06-03 16:48:06 +0300

    Fix top_orders functions

--- entities/library.rb
+++ entities/library.rb
@@ -38,19 +38,18 @@ class Library
   end
 
   def top_readers(quantity = 1)
-    top_order.keys.map(&:reader).last(quantity)
+    top_orders.keys.map(&:reader).last(quantity)
   end
 
   def top_books(quantity = 1)
-    top_order.keys.map(&:book).last(quantity)
+    top_orders.keys.map(&:book).last(quantity)
   end
 
-  def top_order
+  def top_orders
     rating = Hash.new(0)
 
     @orders.each { |order| rating.store(order, rating[order] + 1) }
-    rating.sort_by { |_order, count| count }
-    rating
+    rating.sort_by { |_order, count| count }.to_h
   end
 
   def number_reader_top_books(quantity = 3)
